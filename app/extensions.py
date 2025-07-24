4c32ebe5507a2e34a8fd25e8dd03c09cae2f268b
James Dam <jamesdam2005@gmail.com>
2025-07-24 07:50:36 +0000

    update extensions to include Migrate

--- app/extensions.py
+++ app/extensions.py
@@ -2,10 +2,17 @@
 # These extensions are used for db management,
 # user authentication, and password hashing
 
+
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
+# Primary database
 db = SQLAlchemy()
+
+
+# Other extensions
 login_manager = LoginManager()
 bcrypt = Bcrypt()
+migrate = Migrate()
