8a8fe0607e1f05a490e867ea8cedbd3e59408437
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-28 13:12:21 -0400

    Moved showers folder

--- showers/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is the blueprint initialization for home page routes
-
-from flask import Blueprint
-
-# Create a Blueprint for routes relating to showers
-shower_bp = Blueprint('showers', __name__)
-
-# Importing routes so that they register with the blueprint
-from . import routes
