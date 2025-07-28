0d2e2a55a9fa977933c85e45811680ec8af91ccf
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-28 14:33:09 -0400

    Create times for each shower

--- app/showers/routes.py
+++ app/showers/routes.py
@@ -10,8 +10,17 @@ def shower_list():
 # 
 @shower_bp.route('/showers/<int:shower_id>')
 def shower_schedule(shower_id):
-    # Shows the details of a specific shower
-    return render_template('showers/shower_schedule.html', shower_id=shower_id)
+
+    # Need to make all the possible times for booking
+    times = []
+    for hour in range(24):
+        for minute in [0, 30]:
+            time_str = f"{hour:02}:{minute:02}"
+            times.append(time_str)
+
+    # Shows the details of a specific shower and pass in the time slots
+    return render_template('showers/shower_schedule.html', shower_id=shower_id, 
+                           times = times)
 
 @shower_bp.route('/showers/<int:shower_id>/book', methods=['POST'])
 def book_shower(shower_id):
