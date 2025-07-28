38b1f19a6d07b9b44b496830f630672005669537
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-28 14:39:03 -0400

    Changed times from military to 12hr format

--- app/showers/routes.py
+++ app/showers/routes.py
@@ -1,5 +1,6 @@
 from . import shower_bp
 from flask import render_template, redirect, url_for, request
+from datetime import datetime
 
 
 @shower_bp.route('/showers')
@@ -11,16 +12,17 @@ def shower_list():
 @shower_bp.route('/showers/<int:shower_id>')
 def shower_schedule(shower_id):
 
-    # Need to make all the possible times for booking
+    # Need to make all the possible times for booking | displays in regular 12-hour format
     times = []
     for hour in range(24):
         for minute in [0, 30]:
-            time_str = f"{hour:02}:{minute:02}"
-            times.append(time_str)
+             time_obj = datetime.strptime(f"{hour:02}:{minute:02}", "%H:%M")
+             time_str = time_obj.strftime("%I:%M %p")
+             times.append(time_str)
 
     # Shows the details of a specific shower and pass in the time slots
     return render_template('showers/shower_schedule.html', shower_id=shower_id, 
-                           times = times)
+                           times=times)
 
 @shower_bp.route('/showers/<int:shower_id>/book', methods=['POST'])
 def book_shower(shower_id):
