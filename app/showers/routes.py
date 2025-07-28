5380807915ce6a6a7e9fbb0ec31313d4e6e00ec5
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-28 14:25:21 -0400

    Created booking route - no logic

--- app/showers/routes.py
+++ app/showers/routes.py
@@ -1,8 +1,19 @@
 from . import shower_bp
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 
 @shower_bp.route('/showers')
 def shower_list():
-    # Display the list of showers
-    return render_template('showers.html')
+    # Shows the list of showers
+    return render_template('showers/showers.html')
+
+# 
+@shower_bp.route('/showers/<int:shower_id>')
+def shower_schedule(shower_id):
+    # Shows the details of a specific shower
+    return render_template('showers/shower_schedule.html', shower_id=shower_id)
+
+@shower_bp.route('/showers/<int:shower_id>/book', methods=['POST'])
+def book_shower(shower_id):
+    
+# Gonna implement booking logic here
