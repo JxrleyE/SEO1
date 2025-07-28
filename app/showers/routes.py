a6a13ffc7418f8023fcefb1931167543a841d97a
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-28 15:02:51 -0400

    Seperates times into sections : morning, afternoon, etc

--- app/showers/routes.py
+++ app/showers/routes.py
@@ -5,24 +5,36 @@ from datetime import datetime
 
 @shower_bp.route('/showers')
 def shower_list():
-    # Shows the list of showers
+    # Show the list of showers
     return render_template('showers/showers.html')
 
 # 
 @shower_bp.route('/showers/<int:shower_id>')
 def shower_schedule(shower_id):
 
+    # Validate shower_id is only between 1-4
+    if shower_id < 1 or shower_id > 4:
+        return redirect(url_for('showers.shower_list'))
+    
     # Need to make all the possible times for booking | displays in regular 12-hour format
-    times = []
+    morning = []
+    afternoon = []
+    evening = []
     for hour in range(24):
         for minute in [0, 30]:
              time_obj = datetime.strptime(f"{hour:02}:{minute:02}", "%H:%M")
              time_str = time_obj.strftime("%I:%M %p")
-             times.append(time_str)
+             
+             if hour < 12:
+                 morning.append(time_str)
+             elif hour < 18:
+                 afternoon.append(time_str)
+             else:
+                 evening.append(time_str)
 
     # Shows the details of a specific shower and pass in the time slots
     return render_template('showers/shower_schedule.html', shower_id=shower_id, 
-                           times=times)
+                           morning=morning, afternoon=afternoon, evening=evening)
 
 @shower_bp.route('/showers/<int:shower_id>/book', methods=['POST'])
 def book_shower(shower_id):
