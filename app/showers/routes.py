01f134e98491ed4fec56f061c1b3a09cc75973b2
James Dam <jamesdam2005@gmail.com>
2025-07-30 08:48:38 +0000

    change routes to correct route

--- app/showers/routes.py
+++ app/showers/routes.py
@@ -1,6 +1,7 @@
 from . import shower_bp, forms
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash, session
 from datetime import datetime, timedelta
+from app_queue.services import add_to_queue
 
 # Show the list of showers
 @shower_bp.route('/showers')
@@ -29,20 +30,20 @@ def shower_schedule(shower_id):
              end_time = (time_obj + timedelta(minutes=30)).strftime("%I:%M %p")
              
              # Dictionary to hold both formats
-             time_slot = {
-                 'db_time': db_time,  
-                 'display_time': f"{start_time} - {end_time}"  
+             time_slot_dict = {
+                 'db_value': db_time,  
+                 'display_value': f"{start_time} - {end_time}"  
              }
 
             # Times are determined by the hour
              if hour < 6:
-                 early_morning.append(time_slot['display_time'])
+                 early_morning.append(time_slot_dict)
              elif hour < 12:
-                 morning.append(time_slot['display_time'])
+                 morning.append(time_slot_dict)
              elif hour < 18:
-                 afternoon.append(time_slot['display_time'])
+                 afternoon.append(time_slot_dict)
              else:
-                 evening.append(time_slot['display_time'])
+                 evening.append(time_slot_dict)
 
     # Shows the details of a specific shower and pass in the time slots
     return render_template('showers/shower_schedule.html', shower_id=shower_id,
@@ -50,46 +51,40 @@ def shower_schedule(shower_id):
                              evening=evening)
 
 
-@shower_bp.route('/showers/<int:shower_id>/book', methods=['POST'])
+@shower_bp.route('/showers/<int:shower_id>/book', methods=['GET', 'POST'])
 def book_shower(shower_id):
-    # Gonna implement booking logic here
     form = forms.EventRegistrationForm()
 
+    if request.method == 'POST' and 'time_slot' in request.form:
+        # Get time slot to put into db
+        time_slot = request.form.get('time_slot')
+        time_slot_display = request.form.get('time_slot_display')
+        print(f'TIME SLOT: {time_slot}')
+        print(f'TIME SLOT DISPLAY {time_slot_display}')
+        
+        session['booking'] = {
+            'shower_id': shower_id,
+            'time_slot': time_slot,
+            'time_slot_display': time_slot_display
+        }
+    
+
     if form.validate_on_submit():
+        # Retrieve data from POST request
+        booking_data = session.get('booking')
+
+        time_slot = booking_data['time_slot']
+        time_slot_display = booking_data['time_slot_display']
         phone_number = form.phone_number.data
-        duration = form.duration.data
-        event = form.event.data
+        event = 'shower'
+        duration = 30
 
-        # Get registration time
-        hour = int(form.hour.data)
-        minute = int(form.minute.data)
-        am_pm = form.am_pm.data
-
-        # Adjust for DateTime 24 hour time
-        if am_pm == 'PM' and hour != 12:
-            hour += 12
-        elif am_pm == 'AM' and hour == 12:
-            hour = 0
-
+        # Place info into db
         try:
-            today = datetime.date.today()
- 
-            registration_time = datetime.datetime(
-                today.year, today.month, today.day,
-                hour, minute, 0
-            )
-
-            # Local time zone
-            local_time_zone = datetime.datetime.now().astimezone().tzinfo
-
-            local_registration_time = registration_time.replace(tzinfo=local_time_zone)
-
-            # Convert to UTC
-            registration_time_utc = local_registration_time.astimezone(datetime.timezone.utc)
-
-            add_to_queue(phone_number, event, registration_time_utc, duration)
-            services.send_confirmation_message(phone_number, event, registration_time_utc, duration)
-            return redirect(url_for('sms.test'))
+            add_to_queue(phone_number, event, shower_id, time_slot, duration)
+            # services.send_confirmation_message(phone_number, event, registration_time_utc, duration)
+            flash(f'You have successfully registered to {event} at {time_slot_display}!', 'success')
+            return redirect(url_for('home.dashboard'))
         except Exception as e:
             render_template("showers/register_event.html", form=form, error=e)
             print(e)
