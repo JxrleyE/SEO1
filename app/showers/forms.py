bb3cf05581f43e2b2af08a2b14d4fd7204e49c56
James Dam <jamesdam2005@gmail.com>
2025-07-30 08:48:16 +0000

    add forms for registration

--- app/showers/forms.py
+++ app/showers/forms.py
@@ -1,4 +1,3 @@
-from app_queue.models import QueueEntry
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, IntegerField
 from wtforms.validators import InputRequired, Length, ValidationError, NumberRange
@@ -9,33 +8,5 @@ class EventRegistrationForm(FlaskForm):
     phone_number = StringField(
         validators=[InputRequired(), Length(min=2, max=20)],
         render_kw={"placeholder": "Phone Number (e.g. +12223334444)"}
-    )
-    # Registration time split into 3 parts (hour, minute, AM/PM)
-    hour = SelectField(
-        'Hour',
-        choices=[(str(h), str(h).zfill(2)) for h in range(1, 13)],
-        validators=[InputRequired()]
-    )
-    minute = SelectField(
-        'Minute',
-        choices=[(str(m), str(m).zfill(2)) for m in range(0, 60)],
-        validators=[InputRequired()]
-    )
-    am_pm = SelectField(
-        'AM/PM',
-        choices=[('AM', 'AM'), ('PM', 'PM')],
-        validators=[InputRequired()]
-    )
-    duration = IntegerField(
-        validators=[InputRequired(),
-                    NumberRange(min=10, max=60,
-                    message="Duration must be between 10 and 60 minutes.")
-        ],
-        render_kw={"placeholder": "Duration: (minutes, max 60)"}
-    )
-    event = SelectField(
-        'Shower/Laundry',
-        choices=['Shower', 'Laundry'],
-        validators=[InputRequired()]
     )
     submit = SubmitField('Register')
