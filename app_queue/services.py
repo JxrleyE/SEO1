2618fe61f105cad090ddefbbe9e5678d1d56f0a8
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-07-30 22:29:48 -0400

    Registered times are added to db

--- app_queue/services.py
+++ app_queue/services.py
@@ -1,32 +1,38 @@
 from app import db
 from app_queue.models import QueueEntry
 from sqlalchemy import func, asc
+from datetime import datetime
 
 
 def add_to_queue(phone_number: str, event: str, shower_id: str, registration_time: str, duration: int):
     '''
     Adds user to queue db with position and id
     '''
+
+    # Convert registratoin time back into a from that the db would accept
+    today = datetime.now().strftime('%Y-%m-%d')
+    registration_time = datetime.strptime(f'{today} {registration_time}', '%Y-%m-%d %H:%M:%S')
+
     # Scan for highest position and increment
     max_position = db.session.query(func.max(QueueEntry.position)).filter_by(event_type=event).scalar()
     new_position = (max_position or 0) + 1
 
     # # Check for existing phone number for certain event
-    # existing_entry = QueueEntry.query.filter_by(phone_number=phone_number, event_type=event).first()
-    # if existing_entry:
-    #     raise Exception(f"Phone number {phone_number} is already in the queue for {event} at position {existing_entry.position}.")
+    existing_entry = QueueEntry.query.filter_by(phone_number=phone_number, event_type=event).first()
+    if existing_entry:
+        raise Exception(f"Phone number {phone_number} is already in the queue for {event} at position {existing_entry.position}.")
 
     
-    # new_queue_entry = QueueEntry(
-    #                             phone_number=phone_number,
-    #                             event_type=event,
-    #                             registration_time=registration_time,
-    #                             duration=duration,
-    #                             position=new_position
-    #                             )
+    new_queue_entry = QueueEntry(
+                                phone_number=phone_number,
+                                event_type=event,
+                                registration_time=registration_time,
+                                duration=duration,
+                                position=new_position
+                                )
 
     # # Save to db
-    # db.session.add(new_queue_entry)
-    # db.session.commit()
+    db.session.add(new_queue_entry)
+    db.session.commit()
     print(f'{phone_number} has registered to {event} at shower {shower_id} at time {registration_time} with duration {duration}.')
-    # print(new_queue_entry)
+    print(new_queue_entry)
