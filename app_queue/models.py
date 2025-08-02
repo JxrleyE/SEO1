8a6bdbeb8b9891f1c61cee1aae1714582c70a8c3
Jarley Escobar <jarley@Jarleys-MacBook-Air.local>
2025-08-01 20:06:21 -0400

    Style issues

--- app_queue/models.py
+++ app_queue/models.py
@@ -7,27 +7,31 @@ class QueueEntry(db.Model):
     Information about QueueEntry for Queue db
     '''
     __tablename__ = 'sms_queue'
-    
+
     # Auto incrementing id because phone number can be used twice
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(20), nullable=False, index=True)
     event_type = db.Column(db.String(50), nullable=False)
-    registration_time = db.Column(db.DateTime, nullable=False, default=func.now())
+    registration_time = db.Column(db.DateTime,
+                                  nullable=False,
+                                  default=func.now())
+    # Store as "HH:MM"
     duration = db.Column(db.Integer, nullable=False)
     position = db.Column(db.Integer, nullable=False)
     last_reminder_time = db.Column(db.DateTime, nullable=True)
     shower_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
-    clicked_time = db.Column(db.String(5), nullable=False)  
+    clicked_time = db.Column(db.String(5), nullable=False)
 
-
-     # Define constraints
+    # Define constraints
     __table_args__ = (
-        # A phone number can only register ONCE for a given event_type at a time
-        UniqueConstraint('phone_number', 'event_type', name='uq_phone_number_event_type'),
+        # Phone number can only register ONCE for a given event_type at a time
+        UniqueConstraint('phone_number',
+                         'event_type', name='uq_phone_number_event_type'),
 
         # Position is unique
-        UniqueConstraint('event_type', 'position', name='uq_event_type_position'),
+        UniqueConstraint('event_type',
+                         'position', name='uq_event_type_position'),
 
         # Constraints for duration
         CheckConstraint(duration > 0, name='duration_positive_check'),
@@ -35,5 +39,6 @@ class QueueEntry(db.Model):
     )
 
     def __repr__(self):
-        return f"<QueueEntry Id: {self.id} - PN: {self.phone_number} - Type: {self.event_type} - Time: {self.registration_time} - Pos: {self.position}>"
-
+        return (f"<QueueEntry Id: {self.id} - PN: {self.phone_number} - "
+                f"Type: {self.event_type} - Time: {self.registration_time} - "
+                f"Pos: {self.position}>")
--- app_queue/services.py
+++ app_queue/services.py
@@ -5,50 +5,55 @@ from datetime import datetime
 from flask_login import current_user
 
 
-def add_to_queue(phone_number: str, event: str, shower_id: int, registration_time: str, duration: int, clicked_time):
+def add_to_queue(phone_number: str, event: str, shower_id: int,
+                 registration_time: str, duration: int, clicked_time):
     '''
     Adds user to queue db with position and id
     '''
 
     # Convert registratoin time back into a from that the db would accept
     today = datetime.now().strftime('%Y-%m-%d')
-    registration_time = datetime.strptime(f'{today} {registration_time}', '%Y-%m-%d %H:%M:%S')
+    registration_time = datetime.strptime(
+        f'{today} {registration_time}', '%Y-%m-%d %H:%M:%S')
 
     # Scan for highest position and increment
-    max_position = db.session.query(func.max(QueueEntry.position)).filter_by(event_type=event).scalar()
+    max_position = db.session.query(
+        func.max(QueueEntry.position)).filter_by(event_type=event).scalar()
     new_position = (max_position or 0) + 1
 
-    # # Check for existing phone number for certain event
-    existing_entry = QueueEntry.query.filter_by(phone_number=phone_number, event_type=event).first()
+    # Check for existing phone number for certain event
+    existing_entry = QueueEntry.query.filter_by(
+        phone_number=phone_number, event_type=event).first()
     if existing_entry:
-        raise Exception(f"Phone number {phone_number} is already in the queue for {event} at position {existing_entry.position}.")
+        raise Exception(
+            f"Phone number {phone_number} is already in the queue for {event} "
+            f"at position {existing_entry.position}.")
 
-    
     new_queue_entry = QueueEntry(
-                                phone_number=phone_number,
-                                event_type=event,
-                                registration_time=registration_time,
-                                duration=duration,
-                                position=new_position,
-                                clicked_time=clicked_time,
-                                shower_id=shower_id,
-                                user_id = current_user.id
-                                )
+        phone_number=phone_number,
+        event_type=event,
+        registration_time=registration_time,
+        duration=duration,
+        position=new_position,
+        clicked_time=clicked_time,
+        shower_id=shower_id,
+        user_id=current_user.id
+    )
 
     # Save to db
     db.session.add(new_queue_entry)
     db.session.commit()
-    print(f'{phone_number} has registered to {event} at shower {shower_id} at time {registration_time} with duration {duration}.')
+    print(f'{phone_number} has registered to {event} at shower {shower_id} '
+          f'at time {registration_time} with duration {duration}.')
     print(new_queue_entry)
 
 
- # Check if shower is available at a certain time
+# Check if shower is available at a certain time
 def shower_available(shower_id, time_slot):
 
-   
     booking = QueueEntry.query.filter(
         QueueEntry.shower_id == shower_id,
-        QueueEntry.clicked_time == time_slot, 
+        QueueEntry.clicked_time == time_slot,
         QueueEntry.event_type == 'shower'
     ).first()
 
@@ -57,9 +62,11 @@ def shower_available(shower_id, time_slot):
     else:
         return True
 
+
 # Find and cancels a users booking
 def cancel_queue(queue_id, user_id):
-    booking = QueueEntry.query.filter_by(id=queue_id, user_id=user_id).first()
+    booking = QueueEntry.query.filter_by(
+        id=queue_id, user_id=user_id).first()
 
     if booking:
         db.session.delete(booking)
